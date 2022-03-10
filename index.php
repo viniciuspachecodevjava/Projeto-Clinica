0f242f12f52697e4aa5d4de527e8ef2fb2a338c9
viniciuspch <vinicius.geral2018@gmail.com>
2022-03-10 20:50:48 -0300

    Adicionando arquivos e diretórios ao github

---
+++ index.php
@@ -0,0 +1,291 @@
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=devide-width , user-scalable=0">
+    <link rel="stylesheet" type="text/css" href="assets/styles.css">
+    <title>Medicenter</title>
+    </head>
+    <body>
+       <header>
+          
+        <div class="container">
+        <div class="logo">
+            <a href="index.php"><img src="images/logo1.png" ></a>
+        </div>
+        <div class="menu">
+            <nav>
+                <ul>
+                    <li id="colorhome"><a href="">Home</a></li>
+                    <li><a href="">Clínicas</a></a></li>
+                    <li><a href="#depart">Departamentos</a></li>
+                    <li><a href="">Agenda</a></li>
+                    <li><a href="">Imagens</a></li>
+                    <li><a href="#contact">Contato</a></li>
+                 </ul>
+            </nav>
+        </div>
+        </div>
+       </header>
+        <section id="banner">
+            <div class="container column">
+                <div class="flashytext column">
+                        <h1>Sua saúde em boas mãos</h1>
+                        <h2>O time que entende do assunto</h2>
+                </div>
+                <div class="boxes">
+                    <div class="box1">
+                    <div class="bannertitle">Caso de emergência</div>
+                        <div class="bannerdesc">Se precisar urgentemente fora do horário de funcionamento do Clinicname, 
+                        ligue para o número de agendamento de emergência do serviço de emergência.</div>
+                        <a href="">Leia mais &rarr;</a>
+                    </div>
+                    <div class="box2">
+                    <div class="bannertitle">Horário dos médicos</div>
+                        <div class="bannerdesc">Aqui você tem acesso aos horários de atendimento individuais de cada doutor,
+                        clique em saiba mais logo a baixo para ter acesso a lista.</div>
+                        <a href="">Leia mais &rarr;</a>
+                    </div>
+                    <div class="box3">
+                       <div class="bannertitle">Horários de funcionamento</div>
+                       <div class="date-hour1">
+                            <div>Segunda - Sexta</div><div>8.00h - 17:00h</div>
+                       </div>
+                       <div class="date-hour2">
+                            <div>Sábado</div><div>9:30h - 17:30h</div>
+                       </div>
+                       <div class="date-hour3">
+                            <div>Domingo</div><div>9:30h - 15:00h</div>
+                       </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    <!--Sessão principal do sistema(Meio)-->
+        <section id="second-section">
+            <div class="container">
+                <section id="sub-section">
+                <div class="widget">
+                        <div class="widget-title">
+                        <div class="widget-title-text">Últimas noticias</div>
+                        <div class="widget-bar"> </div>
+                        </div>
+                        <div class="widget-news">
+                        <article>
+                            
+                                <div class="news-data">
+                                <div class="date-article">12 DEC 12</div>
+                                <div class="comments">2</div>
+                            </div>
+                            <a href="">
+                            <div class="news-thumb">
+                                <img src="images/doutor.jpg">
+                            </div>
+                            <div class="news-title">
+                                Lorem Ipsun dolor sit amat velum 
+                            </div>
+                            <div class="news-resume-article">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo. In dapibus sem id metus pharetra facilisis vel at enim. Maecenas tempor aliquam iaculis. Ut a egestas mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis consequat sem sed bibendum.
+                             In ultricies, ligula sit amet molestie pulvinar, magna justo lacinia dui, eget vehicula ipsum nunc vitae lacus. 
+                             Vestibulum sit amet finibus nunc, vel ultricies diam. 
+                             Mauris dui quam, faucibus sit amet sollicitudin non, ultricies at nulla.
+                            </div>
+                            </a>
+                            </article>
+                            <article>
+                            
+                                <div class="news-data">
+                                <div class="date-article">12 DEC 12</div>
+                                <div class="comments">2</div>
+                            </div>
+                            <a href="">
+                            <div class="news-thumb">
+                                <img src="images/doutor.jpg">
+                            </div>
+                            <div class="news-title">
+                                Lorem Ipsun dolor sit amat velum 
+                            </div>
+                            <div class="news-resume-article">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo. In dapibus sem id metus pharetra facilisis vel at enim. Maecenas tempor aliquam iaculis. Ut a egestas mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis consequat sem sed bibendum.
+                             In ultricies, ligula sit amet molestie pulvinar, magna justo lacinia dui, eget vehicula ipsum nunc vitae lacus. 
+                             Vestibulum sit amet finibus nunc, vel ultricies diam. 
+                             Mauris dui quam, faucibus sit amet sollicitudin non, ultricies at nulla.
+                            </div>
+                            </a>
+                            
+                            </article>
+                            <article>
+                            
+                            <div class="news-data">
+                                <div class="date-article">12 DEC 12</div>
+                                <div class="comments">2</div>
+                            </div>
+                            <a href="">
+                            <div class="news-thumb">
+                                <img src="images/doutor.jpg">
+                            </div>
+                            <div class="news-title">
+                                Lorem Ipsun dolor sit amat velum 
+                            </div>
+                            <div class="news-resume-article">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo. In dapibus sem id metus pharetra facilisis vel at enim. Maecenas tempor aliquam iaculis. Ut a egestas mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis consequat sem sed bibendum.
+                             In ultricies, ligula sit amet molestie pulvinar, magna justo lacinia dui, eget vehicula ipsum nunc vitae lacus. 
+                             Vestibulum sit amet finibus nunc, vel ultricies diam. 
+                             Mauris dui quam, faucibus sit amet sollicitudin non, ultricies at nulla.
+                            </div>
+                            </a>
+                            
+                            </article>
+                            <article>
+                            
+                                <div class="news-data">
+                                <div class="date-article">12 DEC 12</div>
+                                <div class="comments">2</div>
+                            </div>
+                            <a href="">
+                            <div class="news-thumb">
+                                <img src="images/doutor.jpg">
+                            </div>
+                            <div class="news-title">
+                                Lorem Ipsun dolor sit amat velum 
+                            </div>
+                            <div class="news-resume-article">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo. In dapibus sem id metus pharetra facilisis vel at enim. Maecenas tempor aliquam iaculis. Ut a egestas mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec iaculis consequat sem sed bibendum.
+                             In ultricies, ligula sit amet molestie pulvinar, magna justo lacinia dui, eget vehicula ipsum nunc vitae lacus. 
+                             Vestibulum sit amet finibus nunc, vel ultricies diam. 
+                             Mauris dui quam, faucibus sit amet sollicitudin non, ultricies at nulla.
+                            </div>
+                            </a>
+                            
+                            </article>
+                        </div>
+                    </div>
+                </section>
+                <aside>
+                    <div id="depart" class="widget">
+                        <div class="widget-title">
+                        <div class="widget-title-text">Departamentos</div>
+                        <div class="widget-bar"></div>
+                        </div>
+                        <div class="widget-body-box">
+                        <div class="widget-box-left">
+                                <div class="widget-box1"></div>
+                                <div class="widget-box2"></div>
+                                <div class="widget-box3"></div>
+                            </div>
+                            <div class="widget-box-right">
+                                <div class="widget-box4"></div>
+                                <div class="widget-box5"></div>
+                                <div class="widget-box6"></div>
+                            </div>
+                        <div id="teste"></div>
+                    </div>
+                    </div>
+                    <div id="contact" class="widget">
+                        <div class="widget-title">
+                        <div class="widget-title-text">Marque uma consulta</div>
+                        <div class="widget-bar"></div>
+                        </div>
+                        <div class="widget-body-appointment">
+                        <div class="desc-appointment">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo.
+                                 In dapibus sem id metus pharetra facilisis vel at enim. Maecenas tempor aliquam iaculis. 
+                            </p>
+                        </div>
+                        <div class="appointment-bar"></div> 
+                        <div class="contact-informations">
+                        <div class="icon-contact1"></div><p>Telefone: <strong>(00)00000-0000</strong></p>
+                        </div>
+                        <div class="appointment-bar"></div>
+                        <div class="contact-informations">
+                        <div class="icon-contact2"></div><p>E-mail: <a href="">medicenter@gmail.com</a></p>
+                        </div>
+                        <div class="appointment-bar"></div>
+                        <div class="contact-informations">
+                        <div class="icon-contact3"></div> <p>Ou <a href="">preencha o formulário</a> em nossa página de contato </p>
+                        </div>
+                        <div class="appointment-bar"></div>
+                        </div>
+                        </div>
+                       
+                    
+                </aside>
+            </div>
+        </section>
+        <footer>
+            <div class="container column">
+                <div class="boxes-footer">
+                <div class="box-footer1">
+                    <a href="">
+                    <h1>Plano de saúde</h1>
+                    <p>Aqui no medicenter temos individual &rarr;</p>
+                    </a>
+                </div>
+                <div class="box-footer2">
+                    <a href="">
+                    <h1>Registros médicos</h1>
+                    <p>Aqui no medicenter temos individual &rarr;</p>
+                    </a>
+                </div>
+                <div class="box-footer3">
+                    <a href="">
+                    <h1>Pagamento de contas</h1>
+                    <p>Aqui no medicenter temos individual &rarr;</p>
+                    </a>
+                </div>
+                </div>
+                <div class="footer-container-area">
+                <div class="colunm-box-footer1">
+                    <h1>Clinica medicenter</h1>
+                    <div class="widget-bar"></div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo. </p>
+                    <div class="colunm-address-contact">
+                    <div class="colunm-address">
+                        <p>Clínica medicenter</p>
+                        <p>34 jorge da costa</p>
+                        <p>Rio Grande do Sul</p>
+                    </div>
+                    <div class="colunm-contact">
+                        <p>+00 0000 0000</p>
+                        <p>+00 0000 0000</p>
+                        <p><span style="color:white;">medicenter@gmail.com<span></p>
+                    </div>
+                    </div>
+                    
+                </div>
+                <div class="colunm-box-footer2">
+                    <h1>Últimas postagens</h1>
+                    <div class="widget-bar"></div>
+                    <div class="colunm-latest-post">
+                        <p>&rarr; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo.</p>
+                        <p>&rarr; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo.</p>
+                        <p>&rarr; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo.</p>
+                    </div>
+                </div>
+                <div class="colunm-box-footer3">
+                    <h1>Últimos tweets</h1>
+                    <div class="widget-bar"></div>
+                    <div class="colunm-latest-tweets">
+                        <p>&rarr; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo.
+                        In ultricies, ligula sit amet molestie pulvinar, magna justo lacinia dui, eget vehicula ipsum nunc vitae lacus. 
+                             Vestibulum sit amet finibus nunc, vel ultricies diam. 
+                        </p>
+                        <p>&rarr; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo.
+                        In ultricies, ligula sit amet molestie pulvinar, magna justo lacinia dui, eget vehicula ipsum nunc vitae lacus. 
+                             Vestibulum sit amet finibus nunc, vel ultricies diam. 
+                        </p>
+                        <p>&rarr; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a mauris justo.
+                        In ultricies, ligula sit amet molestie pulvinar, magna justo lacinia dui, eget vehicula ipsum nunc vitae lacus. 
+                             Vestibulum sit amet finibus nunc, vel ultricies diam. 
+                        </p>
+                    </div>
+                 </div>
+                </div>
+                <div class="copy-footer">
+                    <p>Copyright - <span style="color:white;">Todos os direitos</span> resevados</p>
+                </div>
+            </div>
+        </footer>
+    
+    </body>
+</html>
